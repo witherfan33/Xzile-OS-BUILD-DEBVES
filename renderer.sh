98188996912b144a67fb9035aa375ed3790ffdcf
Wither_Comp12 <97316674+witherfan33@users.noreply.github.com>
2023-01-21 09:50:16 -0600

    Create renderer.sh

---
+++ renderer.sh
@@ -0,0 +1,9 @@
+echo "opening x to z operations"
+
+input "passwd"
+
+if $passwd = 0 then echo "could not verify the users passwd"
+
+  else
+  
+  input = 1 then echo "loading script" while do echo "downloading scripts" then sudo apt install -f
